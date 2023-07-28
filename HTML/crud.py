0da2595ef1694e61997a99f5eb69225650bedbb2
Obie Carnathan <aikia16@uab.edu>
2023-07-27 20:19:02 -0500

    profile photo, user profile

--- HTML/crud.py
+++ HTML/crud.py
@@ -1,20 +1,76 @@
 from main import app, db, User
+import os
+import boto3
+
+ACCESS_KEY ="AKIA6IBZYP2JOCLZAYGT"
+SECRET_KEY ="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"
+AWS_REGION = "us-east-2"
+
+
+def delete_photo_from_s3(photo_url):
+    if not photo_url:
+        return
+
+    # Extract the filename from the photo_url
+    filename = photo_url.split("/")[-1]
+
+    # Create a Boto3 client for S3
+    client = boto3.client(
+        's3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=AWS_REGION
+    )
+    s3 = boto3.resource(        
+        's3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=AWS_REGION
+    )
+    # Currently not working!!!
+    try:
+        client.delete_object(Bucket="artvisionbucket", Key="profilephoto/" + filename)
+        s3.Object("artvisionbucket", "profilephoto/" + filename).delete()
+        print(f"Photo {filename} deleted from S3 bucket")
+    except Exception as e:
+        print(f"Error deleting photo {filename} from S3 bucket: {e}")
+
+def delete_artwork_from_s3(artwork_url):
+    if not artwork_url:
+        return
+
+    # Extract the filename from the artwork_url
+    filename = artwork_url.split("/")[-1]
+
+    # Create a Boto3 client for S3
+    client = boto3.client(
+        's3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=AWS_REGION
+    )
+
+    try:
+        client.delete_object(Bucket="artvisionbucket", Key="artgallery/" + filename)
+        print(f"Artwork {filename} deleted from S3 bucket")
+    except Exception as e:
+        print(f"Error deleting artwork {filename} from S3 bucket: {e}")
 
 # Now, run the database operations within the Flask application context
 with app.app_context():
-    new_User = User('John', 'James','j@gmail.com','JJsmoove','jjsmoove123','I like basketball','profile_photo.jpeg')
-    db.session.add(new_User)
-    db.session.commit()
+    # new_User = User('John', 'James','j@gmail.com','JJsmoove','jjsmoove123','I like basketball','profile_photo.jpeg')
+    # db.session.add(new_User)
+    # db.session.commit()
 
-    # List all Users
-    all_Users = User.query.all()
-    print(all_Users)
+    # # List all Users
+    # all_Users = User.query.all()
+    # print(all_Users)
 
     # Select the User by id
-    first_User = User.query.get(1)
-    if first_User:
-        print(first_User.userName)
-        print(first_User.profilePhotoLink)
+    # first_User = User.query.get(1)
+    # if first_User:
+    #     print(first_User.userName)
+    #     print(first_User.profilePhotoLink)
 
     # # Update
     # first_User = User.query.get(1)
@@ -28,14 +84,26 @@ with app.app_context():
     # db.session.commit()
     all_users = User.query.all()
     for user in all_users:
+        delete_photo_from_s3(user.profilePhotoLink)
+        db.session.delete(user)
+        print(user.profilePhotoLink)
+        print(f"User {user.userName} deleted")
+
+        artworks = Artwork.query.filter_by(user_id=user.id).all()
+        for artwork in artworks:
+            delete_artwork_from_s3(artwork.url)
+            db.session.delete(artwork)
+
+        # Delete the user
         db.session.delete(user)
         print(f"User {user.userName} deleted")
-    db.session.commit()
 
-    new_User = User('Obie', 'C','o@gmail.com','Obie2023','Ambition2023','I like basketball','image/profile_photo.jpeg')
-    db.session.add(new_User)
+
     db.session.commit()
-    print(new_User.id)
-    print(new_User.userName)
-    print(new_User.password)    
-    print(new_User.profilePhotoLink)
+    # new_User = User('Obie', 'C','o@gmail.com','Obie2023','Ambition2023','I like basketball','image/profile_photo.jpeg')
+    # db.session.add(new_User)
+    # db.session.commit()
+    # print(new_User.id)
+    # print(new_User.userName)
+    # print(new_User.password)    
+    # print(new_User.profilePhotoLink)
--- HTML/main.py
+++ HTML/main.py
@@ -1,14 +1,23 @@
 import os
 import re
-from flask import Flask, render_template, session, redirect, url_for
+import datetime
+from flask import Flask, render_template, session, redirect, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import (StringField, SubmitField, BooleanField, DateTimeField,
                      RadioField, SelectField, TextAreaField, DecimalField)
-from flask_wtf.file import FileField, FileAllowed, FileRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired, FileSize
 from wtforms.validators import DataRequired, Length
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import LargeBinary
+import boto3
+from werkzeug.utils import secure_filename
+
+ACCESS_KEY ="AKIA6IBZYP2JOCLZAYGT"
+SECRET_KEY ="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"
+AWS_REGION = "us-east-2"
+
+#artvisionbucket
 
 basedir = os.path.abspath(os.path.dirname(__file__)) 
 #__file__ refers to main.py
@@ -53,7 +62,7 @@ class RegistrationForm(FlaskForm):
     password = StringField(validators = [DataRequired()])
     confirmPassword = StringField(validators = [DataRequired()])
     userName = StringField(validators = [DataRequired()]) 
-    profilePhoto = FileField('Profile Photo', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+    profilePhoto = FileField('Profile Photo', validators=[FileAllowed(['jpg', 'jpeg', 'png']), FileSize(max_size=2 * 1024 * 1024, message='No photos larger than 2MB.')])
     bio = TextAreaField()
     submit = SubmitField('Register Now')
 
@@ -96,13 +105,27 @@ class User(db.Model):
         return f"<User {self.userName}>"
 
 
-# class Artwork(db.Model):
-#     id = db.Column(db.Integer, primary_key = True)
-#     title = db.Column(db.String(80), nullable = False)
-#     description = db.Column(db.String(120), nullable = False)
-#     price = db.Column(db.Float, nullable = False)
-#     status = db.Column(db.String(20), nullable = False)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+class Artwork(db.Model):
+    __tablename__="artworks"
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(80), nullable = False)
+    description = db.Column(db.String(120), nullable = False)
+    price = db.Column(db.Float, nullable = True)
+    status = db.Column(db.String(20), nullable = True)
+    url = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+
+    def __init__(self, title, description, price, status, url, user_id):
+        self.title = title
+        self.description = description
+        self.price = price
+        self.status = status
+        self.url = url
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"<Artwork {self.title}>"
 
 with app.app_context():
     # Create the tables (if not already created)
@@ -127,68 +150,88 @@ def loginPage():
             success = True
             session['logged_in'] = True
             session['user_id'] = user.id
-            return redirect(url_for('userProfile', user_id=user.id))#User's home page
+            return redirect(url_for('userProfile', user_id=user.id)) #User's home page
         else:
             error = "Incorrect Login Info"
 
     return render_template ('loginPage.html', form=form, error=error if 'error' in locals() else None)
 
-@app.route('/registrationPage', methods = ['GET', 'POST'])
-def registrationPage():
-    form = RegistrationForm()
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
+@app.route("/add", methods = ["POST"])
+def add():
     genericPhotoLink = 'image/profile_photo.jpeg'
 
-    if form.validate_on_submit():
-        firstNameInput = form.firstName.data
-        lastNameInput = form.lastName.data
-        emailInput = form.email.data
-        passwordInput = form.password.data
-        confirmPasswordInput = form.confirmPassword.data
-        userNameInput = form.userName.data
-        profilePhotoInput = form.profilePhoto.data
-        bioInput = form.bio.data
-        profilePhotoInput = form.profilePhoto.data
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    confirmpassword = request.form.get("confirmpassword")
+    bio = request.form.get("bio")
+    f = request.files["file"]
+    # filename = f.filename.split("\\")[-1]
+    filename = f"{username}profilepicture.{f.filename.split('.')[-1]}"
+    f.save(secure_filename(filename))
 
-        if not profilePhotoInput:
-            profilePhotoInput=genericPhotoLink
+    client = boto3.client(
+        's3',
+        aws_access_key_id = ACCESS_KEY,
+        aws_secret_access_key = SECRET_KEY,
+        region_name=AWS_REGION
+        )
+    client.upload_file(filename,"artvisionbucket", "profilephoto/" +filename)
+    url = client.generate_presigned_url("get_object",
+        Params={
+            "Bucket":"artvisionbucket",
+            "Key":"profilephoto/"+ filename
+        },
+        ExpiresIn=3600)
 
-        emailCheck = User.query.filter_by(email=emailInput).first()
-        if emailCheck: # If email already exists in database
-            error = "The email you entered is already taken."
-            return render_template ('registrationPage.html', form=form, error=error)
-        
-        userNameCheck = User.query.filter_by(userName=userNameInput).first()
-        if userNameCheck: # If email already exists in database
-            error = "The username you entered is already taken."
-            return render_template ('registrationPage.html', form=form, error=error)
 
-        if not passwordValidation(passwordInput):
-            error = "Password must contain at least one capital letter, one lowercase letter, and end with a number."
-            return render_template('registrationPage.html', form=form, error=error)
-        
-        if passwordInput != confirmPasswordInput:
-            error = "Passwords do not match."
-            return render_template('registrationPage.html', form=form, error=error)
-
-        newUser = User(firstName=firstNameInput, lastName=lastNameInput, email=emailInput, userName=userNameInput, password=passwordInput, bio=bioInput, profilePhotoLink=profilePhotoInput)
-
-        db.session.add(newUser)
-        db.session.commit()
-        return redirect(url_for('loginPage'))
+    emailCheck = User.query.filter_by(email=email).first()
+    if emailCheck: # If email already exists in database
+        error = "The email you entered is already taken."
+        return render_template ('register.html', error=error)
     
-    return render_template ('registrationPage.html', form=form)
+    userNameCheck = User.query.filter_by(userName=username).first()
+    if userNameCheck: # If email already exists in database
+        error = "The username you entered is already taken."
+        return render_template ('register.html', error=error)
+
+    if not passwordValidation(password):
+        error = "Password must contain at least one capital letter, one lowercase letter, and end with a number."
+        return render_template('register.html', error=error)
+    
+    if password != confirmpassword:
+        error = "Passwords do not match."
+        return render_template('register.html', error=error)
+
+    newUser = User(firstName=firstname, lastName=lastname, email=email, 
+        userName=username, password=password, bio=bio, profilePhotoLink=url)
+
+    db.session.add(newUser)
+    db.session.commit()
+    return redirect('loginPage')
+    # return redirect("/")
+
 
 @app.route('/userProfile/<int:user_id>')
 def userProfile(user_id): 
     if success and 'user_id' in session and session['user_id'] == user_id: # 1st person profile visit
         user = User.query.get(user_id)
-        return render_template ('userProfile.html', user=user, isUsersProfile=True)
+        artworks = Artwork.query.filter_by(user_id=user_id).all()
+        
+        return render_template ('userProfile.html', user=user, isUsersProfile=True, artworks=artworks)
 
     # elif success and 'user_id' not in session:
     else: # 3rd person profile visit
         user = User.query.get(user_id)
-        return render_template('userProfile.html', user=user, isUsersProfile=False)    
+        artworks = Artwork.query.filter_by(user_id=user_id).all()
+
+        return render_template('userProfile.html', user=user, isUsersProfile=False, artworks=artworks)    
 
     # else:
     #     return redirect(url_for('error404'))
@@ -208,16 +251,58 @@ def explore():
 @app.route('/uploadPage/<int:user_id>', methods = ['GET', 'POST'])
 def uploadPage(user_id):
     user = User.query.get(user_id)
+    return render_template('upload.html', user=user, user_id=user_id)
 
-    form = UploadForm()
-    if form.validate_on_submit():
-        if form.submit.data:
-            # Save the form data as 'Published'
-            return 'Artwork saved'
-        elif form.saveDraft.data:
-            # Save the form data as 'draft'
-            return 'Artwork saved as draft'
-    return render_template('upload.html', user=user, form=form)
+@app.route("/addArt/<int:user_id>", methods = ["POST"])
+def addArt(user_id):
+    user = User.query.get(user_id)
+
+    title = request.form.get("title")
+    description = request.form.get("description")
+    price = request.form.get("price")
+    status = request.form.get("status")
+
+    f = request.files["file"]
+    filename = f.filename.split("\\")[-1]
+    f.save(secure_filename(filename))
+
+    client = boto3.client(
+        's3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=AWS_REGION
+    )
+
+    # Upload the file to S3 bucket
+    client.upload_file(filename, "artvisionbucket", "artgallery/" + filename)
+    url = client.generate_presigned_url("get_object",
+        Params={
+            "Bucket":"artvisionbucket",
+            "Key":"artgallery/"+ filename
+        },
+        ExpiresIn=3600)
+
+    newArt = Artwork(title =title, description=description, price=price, status=status, url=url, user_id=user_id)
+    db.session.add(newArt)
+    db.session.commit()
+    return redirect(url_for('userProfile', user_id=user_id))
+
+# @app.route('/uploadPage/<int:user_id>', methods = ['GET', 'POST'])
+# def uploadPage(user_id):
+#     user = User.query.get(user_id)
+
+#     form = UploadForm()
+#     if form.validate_on_submit():
+#         if form.submit.data:
+#             # Save the form data as 'Published'
+#             f = form.fileinput.data
+#             filename = secure_filename(f.filename)
+#             artwork_url = "https://artvisionbucket/" + filename
+#             return 'Artwork saved'
+#         elif form.saveDraft.data:
+#             # Save the form data as 'draft'
+#             return 'Artwork saved as draft'
+#     return render_template('upload.html', user=user, form=form)
 
 #Selling Page
 @app.route('/sellingPage/<int:user_id>', methods=['GET', 'POST'])
@@ -245,9 +330,76 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
     # app.run(debug=True)
 
 #home page or domain is locally represented as http://127.0.0.1:5000/
 # to create multiple pages we will use decorators;
 #  @app.route("/another-page")
+
+
+
+
+
+
+
+# @app.route('/registrationPage', methods = ['GET', 'POST'])
+# def registrationPage():
+#     form = RegistrationForm()
+
+#     genericPhotoLink = 'image/profile_photo.jpeg'
+
+#     if form.validate_on_submit():
+#         firstNameInput = form.firstName.data
+#         lastNameInput = form.lastName.data
+#         emailInput = form.email.data
+#         passwordInput = form.password.data
+#         confirmPasswordInput = form.confirmPassword.data
+#         userNameInput = form.userName.data
+#         # profilePhotoInput = form.profilePhoto.data
+#         bioInput = form.bio.data
+#         # profilePhotoInput = form.profilePhoto.data
+
+#         f = request.files["file"]
+#         filename = f.filename.split("\\")[-1]
+#         f.save(secure_filename(filename))
+
+
+#         client = boto3.client(
+#             's3',
+#             aws_access_key_id = ACCESS_KEY,
+#             aws_secret_access_key = SECRET_KEY,
+#             region_name=AWS_REGION
+#             )
+#         client.upload_file(filename,"artvisionbucket", "images/" +filename)
+
+#         # else:
+#         #     profile_Photo_Link = genericPhotoLink           
+
+
+#         emailCheck = User.query.filter_by(email=emailInput).first()
+#         if emailCheck: # If email already exists in database
+#             error = "The email you entered is already taken."
+#             return render_template ('registrationPage.html', form=form, error=error)
+        
+#         userNameCheck = User.query.filter_by(userName=userNameInput).first()
+#         if userNameCheck: # If email already exists in database
+#             error = "The username you entered is already taken."
+#             return render_template ('registrationPage.html', form=form, error=error)
+
+#         if not passwordValidation(passwordInput):
+#             error = "Password must contain at least one capital letter, one lowercase letter, and end with a number."
+#             return render_template('registrationPage.html', form=form, error=error)
+        
+#         if passwordInput != confirmPasswordInput:
+#             error = "Passwords do not match."
+#             return render_template('registrationPage.html', form=form, error=error)
+
+#         newUser = User(firstName=firstNameInput, lastName=lastNameInput, email=emailInput, 
+#             userName=userNameInput, password=passwordInput, bio=bioInput, profilePhotoLink=genericPhotoLink)
+
+#         db.session.add(newUser)
+#         db.session.commit()
+#         return redirect(url_for('loginPage'))
+    
+#     return render_template ('registrationPage.html', form=form)
